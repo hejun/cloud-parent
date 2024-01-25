9a0eb1d1a00a497f184a2891c0394315e0776590
HeJun <1255524587@qq.com>
2024-01-25 23:04:26 +0800

    退出登录

--- cloud-auth/src/main/java/io/github/hejun/cloud/auth/configuration/WebSecurityConfiguration.java
+++ cloud-auth/src/main/java/io/github/hejun/cloud/auth/configuration/WebSecurityConfiguration.java
@@ -3,12 +3,15 @@ package io.github.hejun.cloud.auth.configuration;
 import io.github.hejun.cloud.auth.properties.CorsProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * Web安全配置
@@ -30,7 +33,11 @@ public class WebSecurityConfiguration {
 		http
 			.authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
 			.cors(cors -> cors.configurationSource(corsProperties.build()))
-			.formLogin(login -> login.loginPage("/login").permitAll());
+			.formLogin(login -> login.loginPage("/login").permitAll())
+			.logout(logout -> {
+				logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout", HttpMethod.GET.name()));
+				((SimpleUrlLogoutSuccessHandler) logout.getLogoutSuccessHandler()).setTargetUrlParameter("redirect_uri");
+			});
 		return http.build();
 	}
 
