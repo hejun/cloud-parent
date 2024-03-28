43b15c89d29c457fab823fec1eb7d6da3c27274c
HeJun <1255524587@qq.com>
2024-03-28 21:51:09 +0800

    基础路由

---
+++ cloud-gateway/src/main/java/io/github/hejun/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package io.github.hejun.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 路由模块
+ *
+ * @author HeJun
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
---
+++ cloud-gateway/src/main/java/io/github/hejun/cloud/gateway/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package io.github.hejun.cloud.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * Web安全配置
+ *
+ * @author HeJun
+ */
+@Configuration
+@EnableWebFluxSecurity
+public class WebSecurityConfig {
+
+	@Bean
+	public SecurityWebFilterChain chain(ServerHttpSecurity http) {
+		http
+			.authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+			.oauth2Client(Customizer.withDefaults());
+		return http.build();
+	}
+}
