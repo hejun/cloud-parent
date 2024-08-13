fd93d79df8000df0fe2c1aee5af4c6747de80e79
HeJun <1255524587@qq.com>
2024-08-13 14:16:31 +0800

    基础WebSocket连接

--- cloud-msg/cloud-msg-server/src/main/java/io/github/hejun/cloud/msg/endpoint/WsMessageEndpoint.java
+++ cloud-msg/cloud-msg-server/src/main/java/io/github/hejun/cloud/msg/endpoint/WsMessageEndpoint.java
@@ -1,12 +1,14 @@
 package io.github.hejun.cloud.msg.endpoint;
 
+import io.github.hejun.cloud.msg.util.WsPoolManager;
 import jakarta.websocket.*;
 import jakarta.websocket.server.ServerEndpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.io.IOException;
 
 /**
  * WebSocket连接端点
@@ -18,18 +20,27 @@ import java.util.concurrent.ConcurrentHashMap;
 @ServerEndpoint(value = "/ws")
 public class WsMessageEndpoint {
 
-	private final Map<String, Session> sessions = new ConcurrentHashMap<>();
+	private final String KEY_TOKEN = "token";
 
 	@OnOpen
-	public void onOpen(Session session) {
-		log.debug("Session Open: {}", session.getId());
-		sessions.put(session.getId(), session);
+	public void onOpen(Session session) throws IOException {
+		String token = CollectionUtils.firstElement(session.getRequestParameterMap().get(KEY_TOKEN));
+		if (StringUtils.hasText(token)) {
+			log.debug("Session Open: {}, token: {}", session.getId(), token);
+			WsPoolManager.put(token, session);
+		} else {
+			log.debug("Session [ {} ] miss token, refuse connect.", session.getId());
+			session.close(new CloseReason(CloseReason.CloseCodes.NORMAL_CLOSURE, "Missing token"));
+		}
 	}
 
 	@OnClose
 	public void onClose(Session session, CloseReason closeReason) {
+		String token = CollectionUtils.firstElement(session.getRequestParameterMap().get(KEY_TOKEN));
+		if (StringUtils.hasText(token)) {
+			WsPoolManager.remove(token);
+		}
 		log.debug("Session Close: {}, Reason: {}", session.getId(), closeReason);
-		sessions.remove(session.getId());
 	}
 
 	@OnError
--- cloud-msg/cloud-msg-server/src/main/java/io/github/hejun/cloud/msg/service/impl/WsMsgServiceImpl.java
+++ cloud-msg/cloud-msg-server/src/main/java/io/github/hejun/cloud/msg/service/impl/WsMsgServiceImpl.java
@@ -1,8 +1,14 @@
 package io.github.hejun.cloud.msg.service.impl;
 
+import io.github.hejun.cloud.common.exception.StatefulException;
 import io.github.hejun.cloud.msg.common.dto.Msg;
 import io.github.hejun.cloud.msg.common.enums.MsgType;
 import io.github.hejun.cloud.msg.service.MsgService;
+import io.github.hejun.cloud.msg.util.WsPoolManager;
+import jakarta.websocket.Session;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,12 +16,23 @@ import org.springframework.stereotype.Service;
  *
  * @author HeJun
  */
+@Slf4j
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class WsMsgServiceImpl implements MsgService {
 
 	@Override
 	public String send(Msg msg) throws Exception {
-		return "";
+		Session session = WsPoolManager.get(msg.getReceiver());
+		if (session != null) {
+			synchronized (session) {
+				log.debug("WsMsg: sender [ {} ] send msg to receiver [ {} ] with content: {}", msg.getSender(), msg.getReceiver(), msg.getContent());
+				session.getBasicRemote().sendText(msg.getContent());
+			}
+			return null;
+		}
+		log.debug("WsMsg: receiver [ {} ] is not exists", msg.getReceiver());
+		throw new StatefulException(404, "Session not exists");
 	}
 
 	@Override
